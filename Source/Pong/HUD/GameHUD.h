71726e8eda9a35bddca47f5cda722ab53a9a4957
Scott Adams <sradms0@protonmail.com>
2020-09-01 14:47:27 -0400

    add docstrings to GameHUD

--- Source/Pong/HUD/GameHUD.h
+++ Source/Pong/HUD/GameHUD.h
@@ -6,23 +6,25 @@
 #include "GameFramework/HUD.h"
 #include "GameHUD.generated.h"
 
-/**
- * 
- */
+/** HUD for displaying display game info within widgets in game */
 UCLASS()
 class PONG_API AGameHUD final : public AHUD
 {
 	GENERATED_BODY()
 
 public:
+    /** Initializes score widget-blueprint class. */
     AGameHUD();
-    
+
+    /** Creates the score widget and adds it to the viewport. */
     virtual void BeginPlay() override;
 
 private:
+    /** Displays the scores of both the player and AI */
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player Scores", meta = (AllowPrivateAccess = "true"))
     TSubclassOf<UUserWidget> ScoresWidgetClass;
     
+    /** Contains newly created widgets to add to the viewport */
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player Scores", meta = (AllowPrivateAccess = "true"))
     class UUserWidget* CurrentWidget;
 };
